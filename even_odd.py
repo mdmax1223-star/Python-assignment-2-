b13356a156c502e96729762ea41894476b501d40
Madhan T S <mdmax1223@gmail.com>
2025-08-27 19:05:45 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,11 @@
+
+#Take users input
+
+number=int(input("Enter a integer: "))
+
+# Check if Even or odd
+if number % 2 == 0:
+    print(f"The number {number} is Even.")
+else:
+    print(f"The number {number} id odd,")
+
