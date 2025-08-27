b2b2b507247cbad8557bd360dd45cbe6912ac78a
Madhan T S <mdmax1223@gmail.com>
2025-08-27 19:10:37 +0530

    Add files via upload

---
+++ sum_of_integers.py
@@ -0,0 +1,14 @@
+#sum 1 to 50
+#Initializing sum
+
+total_sum = 0
+
+#loop from 1 to 50
+for i in range(1,51):
+    total_sum +=i
+
+
+
+#Display the final sum
+print(f"The sum of integers from 1 to 50 is: {total_sum}")
+
